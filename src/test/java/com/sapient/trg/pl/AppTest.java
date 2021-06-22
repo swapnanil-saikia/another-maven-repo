942975937cf21f85f7f703554736dc2fe434b8f6
swapnanil saikia <swapnanilsaikia1998@gmail.com>
2021-06-22 15:44:04 +0530

    first

---
+++ src/test/java/com/sapient/trg/pl/AppTest.java
@@ -0,0 +1,17 @@
+package com.sapient.trg.pl;
+
+
+import org.junit.jupiter.api.Test;
+
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+ 
+	@Test
+	public void testApp() {
+		App.main(null);
+	}
+}
